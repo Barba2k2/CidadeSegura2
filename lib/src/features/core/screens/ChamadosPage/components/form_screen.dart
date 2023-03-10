65f7e5d7fde0c0dc01ea86e4c366f91be510175b
Barba2k2 <lorenzo.dz@hotmail.com>
2023-03-10 15:47:55 -0300

    Removido imports nao usados

--- lib/src/features/core/screens/ChamadosPage/components/form_screen.dart
+++ lib/src/features/core/screens/ChamadosPage/components/form_screen.dart
@@ -1,9 +1,7 @@
-import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../../../../../constants/colors_constants.dart';
-import '../../../../../constants/text_strings.dart';
 import 'buttons/cancel_button.dart';
 import 'buttons/send_form_button.dart';
 import 'widgets/modal_alert.dart';
@@ -24,14 +22,17 @@ class _FormScreenState extends State<FormScreen> {
   final _pontoReferenciaController = TextEditingController();
   final _descricaoController = TextEditingController();
 
-  final _formKey = GlobalKey<FormState>(); //Cria uma chave global para acessar o estado do formulário
-  final _formData = <String, Object>{}; //Cria um mapa vazio que será usado para armazenar os dados do formulário.
+  final _formKey = GlobalKey<
+      FormState>(); //Cria uma chave global para acessar o estado do formulário
+  final _formData = <String,
+      Object>{}; //Cria um mapa vazio que será usado para armazenar os dados do formulário.
 
-  final FirebaseFirestore _firestore =
-      FirebaseFirestore.instance; //Cria uma instância do Firestore, para interagir com o banco de dados Firestore do Firebase.
+  final FirebaseFirestore _firestore = FirebaseFirestore
+      .instance; //Cria uma instância do Firestore, para interagir com o banco de dados Firestore do Firebase.
 
   //Salva os dados do formulario no subcolection 'chamados' dentro do documento do usuário no Firestore
-  void saveData(String name, String telefone, String endereco, String bairro, String cidade, String pontoReferencia, String descricao) {
+  void saveData(String name, String telefone, String endereco, String bairro,
+      String cidade, String pontoReferencia, String descricao) {
     CollectionReference users = _firestore.collection('Users');
     DocumentReference userDocRef = users.doc('user_id');
     CollectionReference chamados = userDocRef.collection('chamados');
@@ -78,7 +79,8 @@ class _FormScreenState extends State<FormScreen> {
                       labelText: 'Nome do Solicitante',
                       helperText: 'Seu nome é muito importante!',
                       hintText: 'João Da Silva Pedreira',
-                      prefixIcon: Icon(Icons.person_rounded, color: isDark ? white : black),
+                      prefixIcon: Icon(Icons.person_rounded,
+                          color: isDark ? white : black),
                       hoverColor: black,
                       focusedBorder: const OutlineInputBorder(
                         borderSide: BorderSide(
@@ -128,7 +130,8 @@ class _FormScreenState extends State<FormScreen> {
                       labelText: 'Telefone',
                       helperText: 'Seu telefone tambem é importante!',
                       hintText: '(11) 93551-1525',
-                      prefixIcon: Icon(Icons.phone_sharp, color: isDark ? white : black),
+                      prefixIcon: Icon(Icons.phone_sharp,
+                          color: isDark ? white : black),
                       hoverColor: Colors.red,
                       focusedBorder: const OutlineInputBorder(
                         borderSide: BorderSide(
@@ -166,14 +169,16 @@ class _FormScreenState extends State<FormScreen> {
                     //   return null;
                     // },
                     controller: _telefoneController,
-                    onSaved: (telefone) => _formData['telefone'] = double.parse(telefone ?? ''),
+                    onSaved: (telefone) =>
+                        _formData['telefone'] = double.parse(telefone ?? ''),
                   ),
                   const SizedBox(height: 10),
                   TextFormField(
                     decoration: InputDecoration(
                       labelText: 'Endereço',
                       hintText: 'Rua Augusta, 55',
-                      prefixIcon: Icon(Icons.add_road_sharp, color: isDark ? white : black),
+                      prefixIcon: Icon(Icons.add_road_sharp,
+                          color: isDark ? white : black),
                       hoverColor: whiteGrey,
                       focusedBorder: const OutlineInputBorder(
                         borderSide: BorderSide(
@@ -201,14 +206,16 @@ class _FormScreenState extends State<FormScreen> {
                     minLines: 1,
                     maxLines: null,
                     controller: _enderecoController,
-                    onSaved: (endereco) => _formData['Endereço'] = endereco ?? '',
+                    onSaved: (endereco) =>
+                        _formData['Endereço'] = endereco ?? '',
                   ),
                   const SizedBox(height: 10),
                   TextFormField(
                     decoration: InputDecoration(
                       labelText: 'Bairro',
                       hintText: 'Jardim Anápolis',
-                      prefixIcon: Icon(Icons.location_city_sharp, color: isDark ? white : black),
+                      prefixIcon: Icon(Icons.location_city_sharp,
+                          color: isDark ? white : black),
                       hoverColor: whiteGrey,
                       focusedBorder: const OutlineInputBorder(
                         borderSide: BorderSide(
@@ -246,7 +253,8 @@ class _FormScreenState extends State<FormScreen> {
                     decoration: InputDecoration(
                       labelText: 'Cidade',
                       hintText: 'São Paulo',
-                      prefixIcon: Icon(Icons.account_balance_sharp, color: isDark ? white : black),
+                      prefixIcon: Icon(Icons.account_balance_sharp,
+                          color: isDark ? white : black),
                       hoverColor: whiteGrey,
                       focusedBorder: const OutlineInputBorder(
                         borderSide: BorderSide(
@@ -285,7 +293,8 @@ class _FormScreenState extends State<FormScreen> {
                     decoration: InputDecoration(
                       labelText: 'Ponto de Referencia',
                       hintText: 'Boteko do Seu Zézinho',
-                      prefixIcon: Icon(Icons.add_business_sharp, color: isDark ? white : black),
+                      prefixIcon: Icon(Icons.add_business_sharp,
+                          color: isDark ? white : black),
                       hoverColor: whiteGrey,
                       focusedBorder: const OutlineInputBorder(
                         borderSide: BorderSide(
@@ -316,7 +325,9 @@ class _FormScreenState extends State<FormScreen> {
                     //   return null;
                     // },
                     controller: _pontoReferenciaController,
-                    onSaved: (pontoReferencia) => _formData['Ponto de Referencia'] = pontoReferencia ?? '',
+                    onSaved: (pontoReferencia) =>
+                        _formData['Ponto de Referencia'] =
+                            pontoReferencia ?? '',
                   ),
                   const SizedBox(height: 10), //Espaçamento
                   TextFormField(
@@ -325,7 +336,8 @@ class _FormScreenState extends State<FormScreen> {
                       labelText: 'Descrição do problema', //Rotúlo
                       hintText: 'Luz do poste queimada', //Dica de texto
                       //Muda a cor do icone de acordo com o tema do dispositivo do usuario
-                      prefixIcon: Icon(Icons.abc_sharp, color: isDark ? white : black),
+                      prefixIcon:
+                          Icon(Icons.abc_sharp, color: isDark ? white : black),
                       hoverColor: whiteGrey,
                       focusedBorder: const OutlineInputBorder(
                         //Define o estilo da borda do campo selecionado
@@ -353,7 +365,8 @@ class _FormScreenState extends State<FormScreen> {
                       labelStyle: TextStyle(color: isDark ? white : black),
                     ),
                     minLines: 1, //Define o minimo de linhas do campo de texto
-                    maxLines: null, //Permite que expanda conforme o ususario introduz o texto
+                    maxLines:
+                        null, //Permite que expanda conforme o ususario introduz o texto
                     //Valida o se o campo está vazio, se vazio pede ao usuario que insira um texto
                     validator: (value) {
                       if (value!.isEmpty) {
@@ -362,7 +375,8 @@ class _FormScreenState extends State<FormScreen> {
                       return null;
                     },
                     controller: _descricaoController,
-                    onSaved: (description) => _formData['description'] = description ?? '',
+                    onSaved: (description) =>
+                        _formData['description'] = description ?? '',
                   ),
                   const SizedBox(height: 15),
                   Row(
